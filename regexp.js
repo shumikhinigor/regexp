e52ee810003c5f3c37552d76bd681f6c92335f62
Igor Shumikhin <52691812+shumikhinigor@users.noreply.github.com>
2022-01-12 22:09:43 +0300

    updated README.md and regexp.js

--- regexp.js
+++ regexp.js
@@ -4,9 +4,9 @@ const stringMatch = 'Ваш заказ оформлен 20.12.2021 с номер
 // Example 1
 const regexp = /(?<=№)\d+/ // с помощью литеральной конструкции
 const regexpConstructor = new RegExp('(?<=№)\\d+') // с помощью конструктора
-console.log(regexp.exec(stringMatch)) // регулярное выражение, как объект
-console.log(stringMatch.match(regexp)) // регулярное выражение, как параметр
-regexpConstructor.sticky
+const resultObject = regexp.exec(stringMatch) // регулярное выражение, как объект
+const resultParameter = stringMatch.match(regexp) // регулярное выражение, как параметр
+
 // Example 2
 // \d => 0-9
 // {} => квантификатор
@@ -17,7 +17,7 @@ regexpConstructor.sticky
 // /№\d+/ => от 1 до бесконечности
 // /№\d?/ => либо есть, либо нет
 const regexpNumber = /№\d+/
-console.log(regexpNumber.exec(stringMatch))
+const resultNumber = regexpNumber.exec(stringMatch)
 
 // Example 3
 // \ - экранирование
@@ -28,7 +28,7 @@ console.log(regexpNumber.exec(stringMatch))
 // [a-b] - от символа a до символа b
 // [^a-b] - не входят от символа a до символа b (любой символ не входящий в диапазон a-b)
 const regexpDate = /(\d{2}\.){2}(19\d{2}|2[0-2]\d{2}|\d{2})(?!\d)/
-console.log(regexpDate.exec(stringMatch))
+const resultDate = regexpDate.exec(stringMatch)
 
 // --- REPLACE ---
 const stringReplace = 'Hello ${name}, your age is ${age}'
@@ -49,12 +49,18 @@ const regexpReplace = [/\${(.+?)}/g, (string, key, index, fullString) => {
     console.log(person[key])
     return person[key]
 }]
-const [arg_1, arg_2] = regexpReplace
+const [ arg_1, arg_2 ] = regexpReplace
 const resultReplace = stringReplace.replace(arg_1, arg_2)
-console.log(resultReplace)
 
 // Example 2
-const stringReplaceDuplicate = "aaaabbdbbbcccccsas"
+const stringReplaceDuplicate = 'aaaabbdbbbcccccsas'
+// ([a-z]) - запоминаем символ
+// \1+ - ссылка на символ (больше одного)
+// $1 - запомнившийся символ
 const regexpReplaceDuplicate = /([a-z])\1+/g
+const resultReplaceDuplicate = stringReplaceDuplicate.replace(regexpReplaceDuplicate, '$1')
 
-console.log(stringReplaceDuplicate.replace(regexpReplaceDuplicate, '$1'))
+// Example 3
+const stringReplaceQuotes = 'foo "bl\\"a\\\\" bar'
+const regexpReplaceQuotes = /(?<quote>['"`])(?<content>(?:\\{2}|\\\k<quote>|.(?!\k<quote>))*)\k<quote>/g
+const resultReplaceQuotes = stringReplaceQuotes.replace(regexpReplaceQuotes, '$<content>')
